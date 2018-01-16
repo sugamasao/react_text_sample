aaedb6c43db76c14ed8694365694cc913ff091c2
sugamasao <sugamasao@gmail.com>
2018-01-17 08:20:06 +0900

    create sample

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component{
-    render(){
-       return(
-           <div>Hello World</div>
-       );
+const container = document.querySelector('.app');
+
+class Conveter {
+    static execute(string) {
+        return string.toUpperCase();
     }
 }
 
-ReactDOM.render(<App />, document.getElementById("content"))
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        text: '',
+        };
+    }
+    render() {
+        const {text} = this.state;
+        return (
+        <div>
+            <input
+            type="text"
+            onChange={(e) => {
+                this.setState({
+                    text: Conveter.execute(e.target.value)
+                });
+            }}
+            />
+            <hr />
+            <p>{text}</p>
+        </div>
+        )
+    }
+}
+ReactDOM.render(<App />, container);
